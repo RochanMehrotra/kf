bb45cc9b2cbee7a4f27d53999453a3f6ec3a0e02
RochanMehrotra <37478680+RochanMehrotra@users.noreply.github.com>
2019-10-22 16:59:59 +0530

    Update pytorch_lstm_pipeline.py

--- pytorch_lstm_pipeline.py
+++ pytorch_lstm_pipeline.py
@@ -28,8 +28,8 @@ def mlp_pipeline(
     )
     
     if __name__ == '__main__':
-    import kfp.compiler as compiler
-    compiler.Compiler().compile(mlp_pipeline, 'pytorch_pipeline.tar.gz')
+         import kfp.compiler as compiler
+         compiler.Compiler().compile(mlp_pipeline, 'pytorch_pipeline.tar.gz')
     
 
 
