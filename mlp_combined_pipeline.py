e700c52f17f90745d4ad0536f608a4a24ee12e2f
RochanMehrotra <37478680+RochanMehrotra@users.noreply.github.com>
2019-10-22 15:20:03 +0530

    Add files via upload

---
+++ mlp_combined_pipeline.py
@@ -0,0 +1,33 @@
+import kfp
+
+def task(volume,
+               step_name='mlp_task',
+              mount_output_to='/data'):
+    return kfp.dsl.ContainerOp(
+        name=step_name,
+        image='rochanmehrotra/testing_kf:kf_mlp_task',
+        command=['python3', '/component/src/task.py'],
+        pvolumes={mount_output_to: volume}
+    )
+
+@kfp.dsl.pipeline(name='mlp pipeline', description='')
+def mlp_pipeline(
+        rok_url,
+        pvc_size='1Gi'):
+    
+    vop = kfp.dsl.VolumeOp(
+        name='create-volume',
+        resource_name='mlp_pipeline',
+        annotations={"rok/origin": rok_url},
+        size=pvc_size
+    )
+    
+    component_1 = task(
+        volume=vop.volume
+    )
+    
+    if __name__ == '__main__':
+    import kfp.compiler as compiler
+    compiler.Compiler().compile(mlp_pipeline, 'mlp_pipeline.tar.gz')
+    
+
