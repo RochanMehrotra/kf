a61379dfd9265acb664f4073dd20896975b2f6a0
RochanMehrotra <37478680+RochanMehrotra@users.noreply.github.com>
2019-10-22 18:47:07 +0530

    Add files via upload

---
+++ c_pipeline.py
@@ -0,0 +1,32 @@
+import kfp
+
+def task(volume,
+         step_name='c_task'
+         ):
+    
+    return kfp.dsl.ContainerOp(
+        name=step_name,
+        image='rochanmehrotra/testing_kf:pytorch_lstm',
+        command=['python3', '/component/src/runner.py'],
+        pvolumes={mount_output_to: volume}
+    )
+
+@kfp.dsl.pipeline(name='c pipeline', description='')
+def mlp_pipeline(
+        rok_url,
+        pvc_size='1Gi'):
+    
+    vop = kfp.dsl.VolumeOp(
+        name='create-volume',
+        resource_name='c_pipeline',
+        annotations={"rok/origin": rok_url},
+        size=pvc_size
+    )
+    
+    component_1 = task(
+        volume=vop.volume
+    )
+    
+    if __name__ == '__main__':
+    import kfp.compiler as compiler
+    compiler.Compiler().compile(mlp_pipeline, 'c_pipeline.tar.gz')
